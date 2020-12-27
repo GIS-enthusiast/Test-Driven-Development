fdbc6fe24213004babdd770c6ae262358faa20f9
GIS-enthusiast <srichnz@gmail.com>
2020-12-27 23:14:09 +0100

    added route tests

--- src/tests/functional/test_users.py
+++ src/tests/functional/test_users.py
@@ -95,5 +95,33 @@ def test_all_users(test_app, test_database, add_user):
     assert "ernie@seasamestreet.com" in data[1]["email"]
 
 
+def test_remove_user(test_app, test_database, add_user):
+    test_database.session.query(User).delete() # clear database
+    user = add_user("user-to-be-removed", "remove-me@seasame.com")
+    client = test_app.test_client()
+    resp_one = client.get("/users")
+    data = json.loads(resp_one.data.decode())
+    assert resp_one.status_code == 200
+    assert len(data) == 1
+
+    resp_two = client.delete(f"/users/{user.id}")
+    data = json.loads(resp_two.data.decode())
+    assert resp_two.status_code == 200
+    assert "remove-me@seasame.com was removed!" in data["message"]
+
+    resp_three = client.get("/users")
+    data = json.loads(resp_three.data.decode())
+    assert resp_three.status_code == 200
+    assert len(data) == 1
+
+
+def test_remove_user_incorrect_id(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.delete("/users/999")
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 404
+    assert "User 999 does not exist" in data["message"]
+
+
 # sudo docker-compose exec api python -m pytest "src/tests"
 # sudo docker-compose exec api python -m pytest "src/tests" --lf  (run tests that last failed)
